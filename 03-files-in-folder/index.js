a0009cbe922ca2a393188be90de425f9e0b11e14
Gena230295 <gena23solov@gmail.com>
2024-01-22 17:09:14 +0300

    feat: add third task

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+const absPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(absPath, (err, allFolder) => {
+  if (err) {
+    console.log(err);
+  } else {
+    allFolder.forEach((el) => {
+      const pathFiles = `${absPath}/${el}`;
+      fs.stat(pathFiles, (err, newEl) => {
+        if (err) {
+          console.log(err);
+        } else {
+          if (newEl.isFile()) {
+            const sizeKb = newEl.size / 1000;
+            const nameFile = el.split('.');
+            const firstName = nameFile.slice(0, nameFile.length - 1).join('.');
+            const extName = nameFile
+              .slice(nameFile.length - 1, nameFile.length)
+              .join('');
+            const finStr = `${firstName} - ${extName} - ${sizeKb}`;
+            console.log(finStr);
+          }
+        }
+      });
+    });
+  }
+});
