b46a5f50aec258137af3516acca5b9b90a8bd853
Gena230295 <gena23solov@gmail.com>
2024-01-22 11:23:32 +0300

    feat: add first task

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+
+const absPath = path.join(__dirname, 'text.txt');
+const fileHtml = fs.createReadStream(absPath);
+fileHtml.on('data', (myFile) => {
+  console.log(String(myFile));
+});
